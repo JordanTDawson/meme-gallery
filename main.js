777da5259fcfb859a6fbc0c2931489e29bb34b9d
Jordan Dawson <jtdawson@icloud.com>
2022-11-29 23:39:33 +0000

    completed meme gallery project.

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-var imageUrls = [
-  'https://i.kym-cdn.com/entries/icons/original/000/013/564/doge.jpg',
-  'https://c.tenor.com/uYP_Nkq8VPsAAAAd/coding-hello-world.gif',
-  'https://wiki.godvillegame.com/images/thumb/7/76/Prap.jpg/298px-Prap.jpg'
-];
+var imageUrls = [];
 
 var $imageUrlInput = document.querySelector('input');
 var $addImageButton = document.querySelector('button');
